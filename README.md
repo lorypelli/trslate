f14a24d0356e5c80a0e42c545cf925a259b4214c
LoryPelli <87276663+LoryPelli@users.noreply.github.com>
2024-01-31 21:44:34 +0100

    added `isValidLang` function

--- README.md
+++ README.md
@@ -14,6 +14,7 @@ const translate = new Translation(schema, first, others, ...)
 this is a code to demostrate how it works, the class accept a minimum of two paramethers, the first one is a schema which indicates the order of objects, the other one is the first object which is used to have at least one object required and for types, you can pass how many params you want because the others paramethers will be interpreted as `others`. Obviously if you pass a new param, you will need to add a new `string` element in the `schema` paramether.
 
 ---
+
 `t` function:<br>
 is the actual function used to translate
 
@@ -25,4 +26,21 @@ const key = ''
 console.log(translate.t(lang, key))
 ```
 
-the `t` function accept two paramethers, the first one is the language you want to translate into and the second one is the object key from which to take the string that will be translated
+this function accept two paramethers, the first one is the language you want to translate into and the second one is the object key from which to take the string that will be translated
+
+---
+
+`isValidLang` function:<br>
+used to check if a language is valid
+
+## Example
+
+```js
+const lang = ''
+const valid = translate.isValidLang(lang)
+if (valid) {
+    // do something...
+}
+```
+
+this function accept one paramether, the language and it checks if the translation in that language was provided, it returns a boolean but typescript doesn't understand that you provided a valid type so you will still need to cast it
--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,11 @@
  * @param first the first translation object that is used to make at least one paramether required and also for types
  * @param others array of others objects for the translation
  */
-export class Translation<T extends object> {
-    private schema: string[];
-    private first: T;
+export class Translation<T extends string, K extends object> {
+    private schema: T[];
+    private first: K;
     private others: object[];
-    constructor(schema: string[], first: T, ...others: object[]) {
+    constructor(schema: T[], first: K, ...others: object[]) {
         this.schema = schema;
         this.first = first;
         this.others = others;
@@ -49,63 +49,65 @@ export class Translation<T extends object> {
      * @param a args to pass that will replace `{}` to make variables working
      * @returns the translated string
      */
-    t(l: string, s: SKey<T>, ...a: string[]) {
+    t(l: T, s: SKey<K>, ...a: string[]) {
         if (!this.schema.includes(l)) {
             return '';
-        } else {
-            if (l == this.schema[0]) {
-                if (this.first && typeof this.first == 'object') {
-                    const arr = s.split('.');
-                    const v = arr.reduce<string | object>((o, k) => {
-                        if (typeof o == 'object' && k in o) {
-                            return o[k as keyof typeof o];
-                        }
-                        return {};
-                    }, this.first);
-                    if (typeof v == 'string') {
-                        if (v.includes('{')) {
-                            let str = v;
-                            let c = 0;
-                            str = v.replace(/\{.*?\}/g, () => {
-                                const r = a[c];
-                                c++;
-                                return r;
-                            });
-                            return str;
-                        }
-                        return v;
+        }
+        if (l == this.schema[0]) {
+            if (this.first && typeof this.first == 'object') {
+                const arr = s.split('.');
+                const v = arr.reduce<string | object>((o, k) => {
+                    if (typeof o == 'object' && k in o) {
+                        return o[k as keyof typeof o];
                     }
+                    return {};
+                }, this.first);
+                if (typeof v == 'string') {
+                    if (v.includes('{')) {
+                        let str = v;
+                        let c = 0;
+                        str = v.replace(/\{.*?\}/g, () => {
+                            const r = a[c];
+                            c++;
+                            return r;
+                        });
+                        return str;
+                    }
+                    return v;
                 }
             }
-            for (let i = 0; i < this.schema.length; i++) {
-                if (l == this.schema[i]) {
-                    for (let c = 0; c < this.others.length; c++) {
-                        if (c == i - 1) {
-                            const arr = s.split('.');
-                            const v = arr.reduce<string | object>((o, k) => {
-                                if (typeof o == 'object' && k in o) {
-                                    return o[k as keyof typeof o];
-                                }
-                                return {};
-                            }, this.others[i - 1]);
-                            if (typeof v == 'string') {
-                                if (v.includes('{')) {
-                                    let str = v;
-                                    let c = 0;
-                                    str = v.replace(/\{.*?\}/g, () => {
-                                        const r = a[c];
-                                        c++;
-                                        return r;
-                                    });
-                                    return str;
-                                }
-                                return v;
+        }
+        for (let i = 0; i < this.schema.length; i++) {
+            if (l == this.schema[i]) {
+                for (let c = 0; c < this.others.length; c++) {
+                    if (c == i - 1) {
+                        const arr = s.split('.');
+                        const v = arr.reduce<string | object>((o, k) => {
+                            if (typeof o == 'object' && k in o) {
+                                return o[k as keyof typeof o];
                             }
+                            return {};
+                        }, this.others[i - 1]);
+                        if (typeof v == 'string') {
+                            if (v.includes('{')) {
+                                let str = v;
+                                let c = 0;
+                                str = v.replace(/\{.*?\}/g, () => {
+                                    const r = a[c];
+                                    c++;
+                                    return r;
+                                });
+                                return str;
+                            }
+                            return v;
                         }
                     }
                 }
             }
         }
+    }
+    isValidLang(l: string): l is T {
+        return this.schema.includes(l as T);
     }
 }
 type SKey<T> = { [K in Extract<keyof T, string>]: T[K] extends string ? K : T[K] extends object ? `${K}.${SKey<T[K]>}` : never }[Extract<keyof T, string>]
